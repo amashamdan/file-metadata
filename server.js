6e3103201825a72a2e68ba62b472937f0c8181c7
Amer Hamdan <amashamdan@gmail.com>
2016-10-13 17:50:04 -0700

    Feat: API fully functional now.
    The user can upload a file and receive its size in the respinse.
    get method sends the user the html file to upload the file.
    post method uses multer package to add the file to the request, and then sends its size in the response.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
+var multer = require("multer");
+/* A local folder (uploads will be created) */
+var upload = multer({dest: "uploads/"});
 var app = express();
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/upload", upload.single("uploadedFile"), function(req, res, next) {
+	console.log(req.file.size);
+	res.send({"Size in bytes": req.file.size, "Size in KB": Number((req.file.size/1024).toFixed(2))});
+	res.end();
+})
 
 // port 3000 used for localhost during development.
 var port = Number(process.env.PORT || 3000)
